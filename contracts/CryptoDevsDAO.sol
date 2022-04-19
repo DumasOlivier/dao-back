7c6ec04392b776741e6a9b95dd7a3e7437ef696a
DumasOlivier <dumas.olivier@outlook.fr>
2022-04-19 22:41:00 +0200

    First part of the CryptoDao contract

--- contracts/CryptoDevsDAO.sol
+++ contracts/CryptoDevsDAO.sol
@@ -2,5 +2,60 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ICryptoDevsNFT.sol";
+import "./interfaces/IFakeNFTMarketplace.sol";
 
-contract CryptoDevsDAO is Ownable {}
+contract CryptoDevsDAO is Ownable {
+    struct Proposal {
+        uint256 nftTokenId;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+        mapping(uint256 => bool) voters;
+    }
+
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public numProposals;
+
+    // Enums
+    enum Vote {
+        YAY, // 0
+        NAY // 1
+    }
+
+    // Interfaces
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    // Constructor
+    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+        nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
+        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+    // Modifiers
+    modifier nftHolderOnly() {
+        require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline > block.timestamp, 'DEADLINE_EXCEEDED');
+        _;
+    }
+
+    /// @dev createProposal allows a CryptoDevsNFT holder to create a new proposal in the DAO
+    /// @param _nftTokenId - the tokenID of the NFT to be purchased from FakeNFTMarketplace if this proposal passes
+    /// @return Returns the proposal index for the newly created proposal
+    function createProposal(uint256 _nftTokenId) external nftHolderOnly returns (uint256) {
+        require(nftMarketplace.available(_nftTokenId), 'NFT_NOT_FOR_SALE');
+        Proposal storage proposal = proposals[numProposals];
+        proposal.nftTokenId = _nftTokenId;
+        proposal.deadline = block.timestamp + 5 minutes;
+
+        numProposals++;
+
+        return numProposals - 1;
+    }
+}
