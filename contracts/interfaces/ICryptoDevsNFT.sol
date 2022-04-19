cfab82ac6406c30be173dd21147fa4362b311510
DumasOlivier <dumas.olivier@outlook.fr>
2022-04-19 22:02:06 +0200

    Interfaces for CryptoDevsNft & FakeNFTMarketplace

---
+++ contracts/interfaces/ICryptoDevsNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface ICryptoDevsNFT {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
---
+++ contracts/interfaces/IFakeNFTMarketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IFakeNFTMarketplace {
+    function getPrice() external view returns (uint256);
+
+    function available(uint256 _tokenId) external view returns (bool);
+
+    function purchase(uint256 _tokenId) external payable;
+}
